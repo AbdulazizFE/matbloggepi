034a0058ea6ba3adc8fc5f18b8c3e06957c53535
abah <abah@GBGPCSIAL1>
2022-10-25 15:56:11 +0200

    First Commit

---
+++ MatBlogg/Models/Pages/ProductPage.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MatBlogg.Models.Blocks;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using MatBlogg.Models.Properties;
+
+namespace MatBlogg.Models.Pages
+{
+    /// <summary>
+    /// Used to present a single product
+    /// </summary>
+    [SiteContentType(
+        GUID = "17583DCD-3C11-49DD-A66D-0DEF0DD601FC",
+        GroupName = Global.GroupNames.Meals)]
+    [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-product.png")]
+    [AvailableContentTypes(
+        Availability = Availability.Specific,
+        IncludeOn = new[] { typeof(StartPage) })]
+    public class ProductPage : StandardPage, IHasRelatedContent
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 330)]
+        [CultureSpecific]
+        [AllowedTypes(new[] { typeof(IContentData) },new[] { typeof(JumbotronBlock) })]
+        public virtual ContentArea RelatedContentArea { get; set; }
+    }
+}
